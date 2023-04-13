fe39edfa75cdf3bf102198fe671d12a921fd3e05
Pradeep Verma <pradeep.verma@revv.co.in>
2023-04-13 16:52:46 +0530

    changing blogs

---
+++ components/home-page/hero.js
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+
+import classes from './hero.module.css';
+
+function Hero() {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src='/images/site/k8-logo.jpg'
+          alt='An image showing Pradeep Verma'
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I'm Pradeep</h1>
+      <p>
+        I blog about development and programming.
+      </p>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ next.config.js
@@ -0,0 +1,13 @@
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {},
+    };
+  }
+
+  return {
+    env: {},
+  };
+};
